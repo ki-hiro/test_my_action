06dfca83bbd0fad0f74932e9efe2f5cff4346b87
ki-hiro <kiyotomo.hiroyasu@mo-t.com>
2021-01-26 14:45:44 +0900

    add input parameters for action

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+const core = require('@actions/core');
+
+async function run() {
+    try {
+        const param1 = core.getInput('param1');
+        const param2 = core.getInput('param2');
+        core.info((new Date()).toTimeString());
+        core.info(`${param1} ${param2} ...`);
+        core.wait(1000);
+        core.info((new Date()).toTimeString());
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
