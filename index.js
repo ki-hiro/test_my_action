428544120cee44ba30f2269d51c8b1eb17905db4
ki-hiro <kiyotomo.hiroyasu@mo-t.com>
2021-01-26 15:10:09 +0900

    log

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ console.log('index.js entry point')
 
 const core = require('@actions/core');
 
+const param1 = core.getInput('param1');
+const param2 = core.getInput('param2');
+console.log(`${param1} ${param2} ...`)
+
 async function run() {
     try {
-        const param1 = core.getInput('param1');
-        const param2 = core.getInput('param2');
         core.info((new Date()).toTimeString());
         core.info(`${param1} ${param2} ...`);
-        console.log(`${param1} ${param2} ...`)
         core.wait(1000);
         core.info((new Date()).toTimeString());
     }
@@ -16,3 +17,5 @@ async function run() {
         core.setFailed(error.message);
     }
 }
+
+console.log(`async end point`);
