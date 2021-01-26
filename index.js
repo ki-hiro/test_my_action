4a416207ded480d3a188c4358ca6acf5b00d1b35
ki-hiro <kiyotomo.hiroyasu@mo-t.com>
2021-01-26 15:12:23 +0900

    test

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-console.log('index.js entry point')
-
 const core = require('@actions/core');
 
 const param1 = core.getInput('param1');
 const param2 = core.getInput('param2');
 console.log(`${param1} ${param2} ...`)
 
-async function run() {
-    try {
-        core.info((new Date()).toTimeString());
-        core.info(`${param1} ${param2} ...`);
-        core.wait(1000);
-        core.info((new Date()).toTimeString());
-    }
-    catch (error) {
-        core.setFailed(error.message);
-    }
-}
-
-console.log(`async end point`);
+// 出力を設定したい場合はsetOutput()らしい(未試験)
+// core.setOutput('hoge', new Date().toTimeString());
